f5873a7bd840feda65763f2b27d45311b7a2628d
Andrew Kishino <AndrewKishino@gmail.com>
2015-09-18 11:22:07 -0700

    Added a default help when typing 'ferd translate'

--- translate.js
+++ translate.js
@@ -2,6 +2,12 @@ var request = require('request');
 
 module.exports = function(ferd) {
 
+  ferd.listen(/ferd translate/i, function(response) {
+    
+    response.send('Instructions: `ferd translate` _`text to translate`_');
+
+  });
+
   ferd.listen(/ferd translate (.*)/i, function(response) {
     var text = response.match[1];
     var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key='+ process.env.TRANSLATE +'&lang=en&text=' + text + '&options=1';
